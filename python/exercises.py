38858d76ae3ea568479576c5d2a3fc132a654fc2
JuanDavidEscalanteCastaneda-Campus <juandaesca5@hotmail.com>
2023-02-07 08:57:56 -0500

    point 4

--- python/exercises.py
+++ python/exercises.py
@@ -1,2 +1,16 @@
 # 4. Que son las expresiones regulares en Python?
+print(" ")
 print("son esxpreciones que permiten ir creando patrones para generer un micro programa que permita dividir segun las instruciones")
+import re
+print(" ")
+cadena="i dont like tomato"
+print(f"texto => {cadena}")
+print(" ")
+buscando="tea"
+print(f"buscar={buscando}")
+print(" ")
+if re.search(buscando,cadena) is not None:
+    print("encontrado")
+
+else:
+    print("no encontrado")
