28276c8cbe1b6abe702bcbb4a15a1001d3ff308c
JuanDavidEscalanteCastaneda-Campus <juandaesca5@hotmail.com>
2023-02-07 13:30:28 -0500

    point 9

--- python/exercises.py
+++ python/exercises.py
@@ -1,17 +1,20 @@
-# 8.Escriba un bloque cualquiera de código en Python en donde
-# utilice 2 condicionales (if) anidados.
+# 9.Construya un algoritmo en Python, que permita ingresar la
+# información de un empleado e imprima el nombre, los
+# apellidos y la antigüedad. Los datos que se deben solicitar
+# son los siguientes:
+# *Nombre * Teléfono *Año de ingreso a la empresa
+# *Apellidos *Edad.
+    
 print(" ")
-print("escribe Raul")
-nombre=input("aca bro --->")
+nom1=input("cual es tu primer nombre:\t")
+nom2=input("cual es tu primer nombre:\t")
+ape1=input("cual es tu primer apellido:\t")
+ape2=input("cual es tu primer apellido:\t")
+cel=input("cual es tu telefono:\t")
+a_ing=int(input("cuando entraste a esta empresa:\t"))
+edad=input("cual es tu edad:\t")
 
-if nombre=="Raul":
-    print("EXACTO!!")
-elif nombre=="raul":
-    print("no bro la r en mayuscula ")
-else:
-    print("haceme caso!!")
-    
-    
-
-    
-
+a_act=2023
+print("NOMBRE: "+nom1+" "+nom2+" "+ape1+" "+ape2)
+print(f"TIEMPO EN ESTA EMPRESA:\t{a_act-a_ing} años")
+print("")
