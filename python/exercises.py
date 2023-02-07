9936074477c28f3cc74f1e7fde7ce21e8568b180
JuanDavidEscalanteCastaneda-Campus <juandaesca5@hotmail.com>
2023-02-07 13:07:55 -0500

    point 7

--- python/exercises.py
+++ python/exercises.py
@@ -1,14 +1,21 @@
-# 6. En sus propias palabras, qué son las funciones
-# preconstruidas y proporcione 2 ejemplos.
+#7. Cuál es la diferencia entre un condicional simple y un
+# condicional compuesto?
 print(" ")
-print("son operaciones hechas con antelacion para poder realizar micro tareas dentro del programa")
+print("las condiciones simples sol las que realisan de cisiones de una sola respuesta si o no encambio las compuestas son las que contienen no solo en un tipo de respuesta el programa sino en ambas respuestas1")
 print(" ")
-print("ejemplo 1")
-print("range(6)")
-ran=range(6)
-print(list(ran))
-print(" ")
-print("ejemplo 2")
-print("print(hola)")
-print("hola")
-print(" ")
+print("-------------------condicionales simples-------------------")
+print("si el valor pasa de 300 entonses sume 40")
+n1=int(input("primer digito:\t"))
+n2=int(input("segundo digito:\t"))
+n3=n1+n2
+if n3 > 300:
+    print(f"{n3+40}")
+print("-------------------condicionales compuestas-------------------")
+print("si el valor pasa de 300 entonses sume 40 si no reste 40")
+n1=int(input("primer digito:\t"))
+n2=int(input("segundo digito:\t"))
+n3=n1+n2
+if n3 > 300:
+    print(f"{n3+40}")
+else:
+    print(f"{n3-40}")
