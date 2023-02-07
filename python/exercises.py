e2cf01281569f3dc7991e25b3fe0c6922c5a6c48
JuanDavidEscalanteCastaneda-Campus <juandaesca5@hotmail.com>
2023-02-07 13:17:33 -0500

    point 8

--- python/exercises.py
+++ python/exercises.py
@@ -1,21 +1,17 @@
-#7. Cuál es la diferencia entre un condicional simple y un
-# condicional compuesto?
+# 8.Escriba un bloque cualquiera de código en Python en donde
+# utilice 2 condicionales (if) anidados.
 print(" ")
-print("las condiciones simples sol las que realisan de cisiones de una sola respuesta si o no encambio las compuestas son las que contienen no solo en un tipo de respuesta el programa sino en ambas respuestas1")
-print(" ")
-print("-------------------condicionales simples-------------------")
-print("si el valor pasa de 300 entonses sume 40")
-n1=int(input("primer digito:\t"))
-n2=int(input("segundo digito:\t"))
-n3=n1+n2
-if n3 > 300:
-    print(f"{n3+40}")
-print("-------------------condicionales compuestas-------------------")
-print("si el valor pasa de 300 entonses sume 40 si no reste 40")
-n1=int(input("primer digito:\t"))
-n2=int(input("segundo digito:\t"))
-n3=n1+n2
-if n3 > 300:
-    print(f"{n3+40}")
+print("escribe Raul")
+nombre=input("aca bro --->")
+
+if nombre=="Raul":
+    print("EXACTO!!")
+elif nombre=="raul":
+    print("no bro la r en mayuscula ")
 else:
-    print(f"{n3-40}")
+    print("haceme caso!!")
+    
+    
+
+    
+
