62db8fd859059bf880e7a573337b03623e70406d
JuanDavidEscalanteCastaneda-Campus <juandaesca5@hotmail.com>
2023-03-06 09:35:04 -0500

    actualizacion

---
+++ estudiojava/main.js
@@ -0,0 +1,10 @@
+const lista={
+    "tradicional (%)":["90-100","80-89","70-79","60-69","<60","0"],
+    "14-point range":["12-14","9-11","6-8","3-52","1-2","0"],
+    "letter":["A","B","C","D","E","Z",],
+    "SBG Rating":[4,3,2,1,0,0],
+    ""
+
+};
+
+nota=prompt("ingresa tu nota")
