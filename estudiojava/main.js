f1d05c41cc413d3fc0f3123858521608de1a53e9
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-07 13:27:25 -0500

    taller javascript 1

--- estudiojava/main.js
+++ estudiojava/main.js
@@ -10,42 +10,78 @@ let tabla=[
 console.table(tabla,[`traditional`,`pointRange`,`letter`,`SBGRating`,`ProficiencyLevel`])
 
  let nota=prompt("ingresa tu nota")
-switch(nota){
-    case (nota >= 90 && nota<=100):
-        console.log(`¡EXELENTE!, tus datos fueron los siguientes:\n
-        calificacion:\tA\n
-        ranking:\t4\n
-        exceeds profienciency`)
-    break;
-    case nota >= 80 && nota<=89:
-        console.log(`¡VAMOOOSS!, tus datos fueron los siguientes:\n
-        calificacion:\tB\n
-        ranking:\t3\n
-        Demonstrates proficiency`)
-    break;
-    case nota >= 70 && nota<=79:
-        console.log(`BIEN, tus datos fueron los siguientes:\n
-        calificacion:\tC\n
-        ranking:\t2\n
-        approaches profienciency`)
-    break;
-    case nota >= 60 && nota<=69:
-        console.log(`DALE TU PUEDES, tus datos fueron los siguientes:\n
-        calificacion:\tD\n
-        ranking:\t1\n
-        falls well below profienciency`)
-    break;
-    case nota > 0 && nota<60:
-        console.log(`ESFORZATE, tus datos fueron los siguientes:\n
-        calificacion:\tE\n
-        ranking:\t0\n
-        no attempt made`)
-    break;
-    case 0:
-        console.log(`PENDEJO, tus datos fueron los siguientes:\n
-        calificacion:\tF\n
-        ranking:\t0\n
-        exceeds profienciency`)
-    break;
-}
+
+ if(nota >= 90 && nota<=100){
+    console.log(`¡EXELENTE!, tus datos fueron los siguientes:\n
+    calificacion:\tA\n
+    ranking:\t\t4\n
+    exceeds profienciency`)
+ } else if (nota >= 80 && nota<=89){
+    console.log(`¡VAMOOOSS!, tus datos fueron los siguientes:\n
+    calificacion:\tB\n
+    ranking:\t\t3\n
+    Demonstrates proficiency`)
+ }else if(nota >= 70 && nota<=79){
+    console.log(`BIEN, tus datos fueron los siguientes:\n
+    calificacion:\tC\n
+    ranking:\t\t2\n
+    approaches profienciency`)
+ }else if(nota >= 60 && nota<=69){
+    console.log(`DALE TU PUEDES, tus datos fueron los siguientes:\n
+    calificacion:\tD\n
+    ranking:\t\t1\n
+    falls well below profienciency`)
+ }else if(nota > 0 && nota<60){
+    console.log(`ESFORZATE, tus datos fueron los siguientes:\n
+    calificacion:\tE\n
+    ranking:\t\t0\n
+    no attempt made`)
+ }else if(nota=0){
+    console.log(`PENDEJO, tus datos fueron los siguientes:\n
+    calificacion:\tF\n
+    ranking:\t\t0\n
+    exceeds profienciency`)
+ }else{
+    console.log("digita un valor de 1 a 100")
+ }
+
+ 
+// switch(nota){
+//     case nota >= 90 && nota<=100:
+//         console.log(`¡EXELENTE!, tus datos fueron los siguientes:\n
+//         calificacion:\tA\n
+//         ranking:\t4\n
+//         exceeds profienciency`)
+//     break;
+//     case nota >= 80 && nota<=89:
+//         console.log(`¡VAMOOOSS!, tus datos fueron los siguientes:\n
+//         calificacion:\tB\n
+//         ranking:\t3\n
+//         Demonstrates proficiency`)
+//     break;
+//     case nota >= 70 && nota<=79:
+//         console.log(`BIEN, tus datos fueron los siguientes:\n
+//         calificacion:\tC\n
+//         ranking:\t2\n
+//         approaches profienciency`)
+//     break;
+//     case nota >= 60 && nota<=69:
+//         console.log(`DALE TU PUEDES, tus datos fueron los siguientes:\n
+//         calificacion:\tD\n
+//         ranking:\t1\n
+//         falls well below profienciency`)
+//     break;
+//     case nota > 0 && nota<60:
+//         console.log(`ESFORZATE, tus datos fueron los siguientes:\n
+//         calificacion:\tE\n
+//         ranking:\t0\n
+//         no attempt made`)
+//     break;
+//     case 0:
+//         console.log(`PENDEJO, tus datos fueron los siguientes:\n
+//         calificacion:\tF\n
+//         ranking:\t0\n
+//         exceeds profienciency`)
+//     break;
+// }
 
---
+++ taller javascript/main.js
@@ -0,0 +1,4 @@
+let dobles="esto es una string de comillas dobles"
+let simples='esto es una string de comillas simples'
+document.getElementById('dobles').innerHTML = `<h1 style="color: red">${dobles}</h1>`
+document.getElementById(simples)
