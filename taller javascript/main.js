40486cdf750d9cfd525acb3aaf2c52410d8e9979
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-08 07:59:57 -0500

    feat: :tada: metodo length
    longitud de strings

--- taller javascript/main.js
+++ taller javascript/main.js
@@ -1,4 +1,15 @@
-let dobles="esto es una string de comillas dobles"
-let simples='esto es una string de comillas simples'
-document.getElementById('dobles').innerHTML = `<h1 style="color: red">${dobles}</h1>`
-document.getElementById(simples)
+
+let MiGranPoema="del cielo cayo una rosa, ayer me comi una oblea y como no soy poeta coman mierda gonorreas";
+console.log(MiGranPoema);
+console.log("podemos medir cualquier string\n");
+console.log("ejemplo:\n");
+console.log(`\teste gran poema contiene ${MiGranPoema.length} caracteres\n`);
+let YouCode=prompt("escribe cualquier cosa");
+console.log(`\t${YouCode} contiene ${YouCode.length} caracteres\n`);
+
+
+
+
+
+
+// document.getElementById('dobles').innerHTML = `<h1 style="color: red">${dobles}</h1>`
