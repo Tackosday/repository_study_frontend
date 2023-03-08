3577e25b24e9aa32bbc03c61092aba6486559c81
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-08 09:19:44 -0500

    feat: :zap: metodo slice
    metodo para extraer partes de una string

--- taller javascript/main.js
+++ taller javascript/main.js
@@ -1,7 +1,6 @@
 
 let MiGranPoema="Del cielo cayo una rosa, ayer me comi una oblea y como no soy poeta coman mierda gonorreas";
 console.log(MiGranPoema);
-console.log("tambien podemos remplazar los caracteres por otros");
-console.log("remplaza la palabra:");
-let YouCode=prompt(`${MiGranPoema} remplaza rosa por que palabra`);
-console.log(MiGranPoema.replace("rosa",YouCode));
+console.log("podemos extraer partes del texto ");
+console.log("MiGranPoema.slice(20,35)")
+console.log(MiGranPoema.slice(20,35))
