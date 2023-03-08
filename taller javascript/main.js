f8a8cad5fa1c44549e567a56d20625dfb20fb502
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-08 09:11:27 -0500

    feat: :zap: metodo remplace
    metodo para remplazar caracteres de string

--- taller javascript/main.js
+++ taller javascript/main.js
@@ -1,32 +1,7 @@
 
 let MiGranPoema="Del cielo cayo una rosa, ayer me comi una oblea y como no soy poeta coman mierda gonorreas";
 console.log(MiGranPoema);
-console.log("podemos corroborar si pertenece algun caracter a la string ya sea al inico al final o en cualquier otro lugar\n");
-let YouCode0=prompt("escribe la primera letra en mayuscula");
-
-if(YouCode0 = MiGranPoema.startsWith){
-    console.log("exacto")
-}else{
-    console.log("nokas")
-}
-
-let YouCode1=prompt("escribe la ultima letra");
-
-if(YouCode1 = MiGranPoema.endsWith){
-    console.log("exacto")
-}else{
-    console.log("nokas")
-}
-
-let YouCode2=prompt("escribe la cualquier letra");
-
-
-
-
-
-if(YouCode0 = MiGranPoema.includes){
-    console.log("si esta")
-}else{
-    console.log("no esta")
-}
-
+console.log("tambien podemos remplazar los caracteres por otros");
+console.log("remplaza la palabra:");
+let YouCode=prompt(`${MiGranPoema} remplaza rosa por que palabra`);
+console.log(MiGranPoema.replace("rosa",YouCode));
