843b899501ab308427c58876858a4fe360f64195
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-08 08:33:39 -0500

    feat: :zap: metodo startWith/endWith/includes
    metodos de busquedas de caracteres

--- taller javascript/main.js
+++ taller javascript/main.js
@@ -1,15 +1,32 @@
 
-let MiGranPoema="del cielo cayo una rosa, ayer me comi una oblea y como no soy poeta coman mierda gonorreas";
+let MiGranPoema="Del cielo cayo una rosa, ayer me comi una oblea y como no soy poeta coman mierda gonorreas";
 console.log(MiGranPoema);
-console.log("podemos medir cualquier string\n");
-console.log("ejemplo:\n");
-console.log(`\teste gran poema contiene ${MiGranPoema.length} caracteres\n`);
-let YouCode=prompt("escribe cualquier cosa");
-console.log(`\t${YouCode} contiene ${YouCode.length} caracteres\n`);
+console.log("podemos corroborar si pertenece algun caracter a la string ya sea al inico al final o en cualquier otro lugar\n");
+let YouCode0=prompt("escribe la primera letra en mayuscula");
+
+if(YouCode0 = MiGranPoema.startsWith){
+    console.log("exacto")
+}else{
+    console.log("nokas")
+}
+
+let YouCode1=prompt("escribe la ultima letra");
+
+if(YouCode1 = MiGranPoema.endsWith){
+    console.log("exacto")
+}else{
+    console.log("nokas")
+}
+
+let YouCode2=prompt("escribe la cualquier letra");
 
 
 
 
 
+if(YouCode0 = MiGranPoema.includes){
+    console.log("si esta")
+}else{
+    console.log("no esta")
+}
 
-// document.getElementById('dobles').innerHTML = `<h1 style="color: red">${dobles}</h1>`
