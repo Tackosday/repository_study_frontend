dcd99ccf68b8e411c8623fcfe14426f0a45da6ab
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-09 07:44:16 -0500

    feat: :zap: metodo charAt
    permite extraer un caracter

--- taller javascript/main.js
+++ taller javascript/main.js
@@ -2,5 +2,5 @@
 let MiGranPoema="Rosa ";
 console.log(MiGranPoema);
 console.log("podemos extraer caracter de una string");
-console.log("MiGranPoema.charAt(0,23)");
-console.log(MiGranPoema.charAt(0,23));
+console.log("MiGranPoema.charAt(2)");
+console.log(MiGranPoema.charAt(2));
