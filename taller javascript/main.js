eff7f561feeb7d6401b7ae9c0d1a7be15936318d
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-08 21:04:17 -0500

    feat: :zap: metodo slice
    permite extraer strings de una string

--- taller javascript/main.js
+++ taller javascript/main.js
@@ -1,6 +1,6 @@
 
 let MiGranPoema="Del cielo cayo una rosa, ayer me comi una oblea y como no soy poeta coman mierda gonorreas";
 console.log(MiGranPoema);
-console.log("podemos extraer partes del texto ");
-console.log("MiGranPoema.slice(20,35)")
-console.log(MiGranPoema.slice(20,35))
+console.log("podemos extraer partes del texto como substring ");
+console.log("MiGranPoema.slice(0,23)");
+console.log(MiGranPoema.slice(0,23));
