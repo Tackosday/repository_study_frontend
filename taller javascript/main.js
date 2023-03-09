0b1da7fd6bacc64441397ae411b01a3fd615df79
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-09 09:33:38 -0500

    feat: :zap: metodo fronCharCode
    permite pasar de numeros unicos a caracteres

--- taller javascript/main.js
+++ taller javascript/main.js
@@ -1,6 +1,5 @@
 
-let MiGranPoema="Rosa ";
-console.log(MiGranPoema);
-console.log("proporciona el codigo de una cadena ");
-console.log("MiGranPoema.codePointAt(1)");
-console.log(MiGranPoema.codePointAt(1));
+console.log("permite pasar de numero inico a caracter");
+console.log("encrip.fromCharCode(104, 101, 108, 108, 111)");
+let str=String.fromCharCode(104, 101, 108, 108, 111);
+console.log(str)
