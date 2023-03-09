35bd40b3f7c81d138d06e47464591fd0f11de48a
JuanDavidEscalateCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-09 13:31:28 -0500

    feat: :zap: metodo indexOf
    permite buscar elementos en objetos, arrays, listas...

--- taller javascript/main.js
+++ taller javascript/main.js
@@ -1,10 +1,7 @@
-let pri="(1)";
-let sec="(2)";
-let ter="(3)";
-console.log("no concatenado")
-console.log('let pri="(1)";\nlet sec="(2)";\nlet ter="(3)";')
-console.log("concatenado")
-const conc="".concat(pri,sec,ter)
-console.log(conc)
-
-
+const array=[
+    pri="(1)",
+    sec="(2)",
+    ter="(3)",
+];
+console.log("indexOf(2)");
+console.log(array.indexOf("(2)"));
