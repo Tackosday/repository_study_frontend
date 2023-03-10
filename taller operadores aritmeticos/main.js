2118291a806de03f995953dcac07b394b5f9b3b9
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-10 08:05:21 -0500

    feat: :zap: operador .min
    permite elegir el numero menor

--- taller operadores aritmeticos/main.js
+++ taller operadores aritmeticos/main.js
@@ -1,3 +1,8 @@
-console.log(Math.PI)
-console.log("Math.PI*10-6")
-console.log(Math.PI*10-6)
+let num0=1;
+let num1=3;
+let num2=6;
+let num3=14;
+let num5=5;
+
+console.log("console.log(Math.min(num0,num1,num2,num3,num5)");
+console.log(Math.min(num0,num1,num2,num3,num5));
