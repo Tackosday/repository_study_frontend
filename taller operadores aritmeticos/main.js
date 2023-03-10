0472957905b4de4419c8deee623941dd19c36fa3
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-10 08:09:19 -0500

    feat: :zap: operador .pow
    permite crear exponentes en numeros

--- taller operadores aritmeticos/main.js
+++ taller operadores aritmeticos/main.js
@@ -1,8 +1,4 @@
-let num0=1;
-let num1=3;
-let num2=6;
-let num3=14;
-let num5=5;
+let num0=5;
+let num1=2;
 
-console.log("console.log(Math.min(num0,num1,num2,num3,num5)");
-console.log(Math.max(num0,num1,num2,num3,num5));
+console.log(Math.pow(num0,num1))
