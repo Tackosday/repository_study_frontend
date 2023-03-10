aedfa6b865d31b31ba00d98bddf1a4dcc6472568
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-10 08:51:14 -0500

    feat: :zap: operador ||
    permite devolver true si obtiente un true de almenos de una condicional

--- taller operadores aritmeticos/main.js
+++ taller operadores aritmeticos/main.js
@@ -1,6 +1,6 @@
-const num=prompt("decime un numero del 1 al diez")
+const num=prompt("no me des un numero del 1 al diez")
 
-if  (num <= 10 && num > 0){
+if  (num > 10 || num <= 0){
     console.log("asi me gusta, que obedesca")    
 }else{
     console.log("quiere que le mande un gota gota?? ")
