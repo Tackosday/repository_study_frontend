e57d64eb397d7e4212a7121ab86d6b2a2f535d00
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-10 07:01:17 -0500

    feat: :zap: metodo split
    permite separar una string

--- taller javascript/main.js
+++ taller javascript/main.js
@@ -1,4 +1,4 @@
-const primVar=" bienvenido jugador a la grieta de invocador ";
+const primVar="bienvenido jugador a la grieta de invocador";
 console.log(primVar);
-console.log("console.log(primVar.trim());");
-console.log(primVar.trim());
+console.log('console.log(primVar.split(" "))');
+console.log(primVar.split(" "));
