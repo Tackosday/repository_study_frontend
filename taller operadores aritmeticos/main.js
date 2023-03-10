4c3c7e3a53d6e6cb29217330667e23d78c5731b4
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-10 07:38:03 -0500

    feat: :zap: operador de decremento
    permite restar uno a uno

--- taller operadores aritmeticos/main.js
+++ taller operadores aritmeticos/main.js
@@ -1,8 +1,8 @@
 let valor0=5;
 let valor1=2;
 
-console.log("const valor00=valor0 ++;\nconst valor10=++ valor1;");
-const valor00=valor0 ++;
-const valor10=++ valor1;
+console.log("const valor00=valor0 --;\nconst valor10=-- valor1;");
+const valor00=valor0--;
+const valor10=--valor1;
 console.log(valor00)
-console.log(valor00)
+console.log(valor10)
