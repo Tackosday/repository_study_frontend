bbb378aa785f4ff508c4a177c186fef62e673e94
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-10 20:37:14 -0500

    feat: :zap: operador esctrictamente igual
    permite crear una igualdad exacta diferenciando asta el tipo de dato

--- taller operadores aritmeticos/main.js
+++ taller operadores aritmeticos/main.js
@@ -1,8 +1,10 @@
-let num0=prompt("coloca un numero");
-let num1=prompt("coloca un numero diferente al anterior");
 
-if(num0!=num1){
-    console.log("perfecto");
+
+let num0="a";
+let num1="A";
+
+if(num0===num1){
+    console.log("es exactamente igual");
 }else{
-    console.log("hazme caso ");
+    console.log("no es exactamente igual");
 }
