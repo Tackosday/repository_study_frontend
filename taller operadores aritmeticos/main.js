b617c5c36ba1eb28c4c53e3ff51de49a98a3bfb7
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-10 20:38:58 -0500

    feat: :zap: operador extrictramente desigual
    permite crear una desigualdad exacta diferenciando asta el tipo de dato o si es mayuscula o minuscula

--- taller operadores aritmeticos/main.js
+++ taller operadores aritmeticos/main.js
@@ -3,8 +3,8 @@
 let num0="a";
 let num1="A";
 
-if(num0===num1){
-    console.log("es exactamente igual");
+if(num0!==num1){
+    console.log("es exactamente desigual");
 }else{
-    console.log("no es exactamente igual");
+    console.log("no es exactamente desigual");
 }
