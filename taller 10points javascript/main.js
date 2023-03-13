079a9c1cc186192af10f04aa2a3be4f3a078a2d9
jose david <ramirezjd1005@gmail.com>
2023-03-13 07:45:48 -0500

    feat: :tada: 1 Punto
    Construir el algoritmo para un programa que ingrese tres notas de un alumno, si el promedio es menor o igual a 3.9 mostrar un mensaje "Estudie“, de lo contrario un mensaje que diga "becado"

---
+++ taller 10points javascript/main.js
@@ -0,0 +1,11 @@
+let num0=parseInt(prompt("ingresa tu numero de nota"));
+let num1=parseInt(prompt("ingresa tu numero de nota"));
+let num2=parseInt(prompt("ingresa tu numero de nota"));
+
+let promedio=(num0+num1+num2)/3
+
+if(promedio<=3.9){
+    console.log("estudie")
+}else{
+    console.log("becado")
+}
