18b58f8ed51804e0dd751d7673d69a4f0c0add1e
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-13 12:53:02 -0500

    feat: :art: 3 Punto
    Construir el algoritmo para determinar el voltaje de un circuito a partir de la resistencia y la intensidad de corriente.

--- taller 10points javascript/main.js
+++ taller 10points javascript/main.js
@@ -1,14 +1,4 @@
-let num0=parseInt(prompt("digita un numero"));
-let nume=num0%2
-let sol;
-if(nume==0){
-    sol="este numero es par";
-}else if(nume=1){
-    sol="este numero no es par";
-}
+let ampe=parseInt(prompt("digita el amperaje"));
+let resi=parseInt(prompt("digita la resistencia"));
 
-if(num0>10){
-    console.log(`este numero es mayor a 10 y ${sol}`);
-}else{
-    console.log(`este numero no es mayor a 10 y ${sol}`);
-}
+console.log(`este es su voltaje ${ampe*resi}V`);
