b290791e91ef246353c51c743fbbcb7494d79c70
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-13 13:03:59 -0500

    feat: :art: 4 Punto
    Construir el algoritmo que solicite el nombre y edad de 3 personas y determine el nombre de la persona con mayor edad.

--- taller 10points javascript/main.js
+++ taller 10points javascript/main.js
@@ -1,4 +1,24 @@
-let ampe=parseInt(prompt("digita el amperaje"));
-let resi=parseInt(prompt("digita la resistencia"));
+let eda0=parseInt(prompt("edad del primer usuario"));
+let eda1=parseInt(prompt("edad del segundo usuario"));
+let eda2=parseInt(prompt("edad del tercero usuario"));
+let mayor;
+let cual;
+if(eda0>eda1){
+    if(eda0>eda2){
+        cual="primero";
+        mayor=eda0;
+    }else{
+        cual="tercero";
+        mayor=eda2;
+    }
+}else{
+    if(eda1>eda2){
+        cual="segundo";
+        mayor=eda1;
+    }else{
+        cual="tercero";
+        mayor=eda2;
+    }
+}
 
-console.log(`este es su voltaje ${ampe*resi}V`);
+console.log(`el usuario con mas años es el ${cual} con ${mayor} años`)
