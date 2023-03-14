d0b57b6a6698b30c3d35090aa0ea36c5242d89b1
JuanDavidEscalanteCastañeda <juandaesca5@hotmail.com>
2023-03-14 07:43:33 -0500

    feat: :art: 7 Punto
    Programa que pida el ingreso del nombre y precio de un artículo y la cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador en su factura

--- taller 10points javascript/main.js
+++ taller 10points javascript/main.js
@@ -1,21 +1,5 @@
-let bool=true;
-let nombres=[];
-let genero=[];
-let final=[];
+let nombre=prompt("cual es tu nombre");
+let costo=parseInt(prompt("cuanto vale el producto??"));
+let cuantos=parseInt(prompt("cuantos productos lleva??"));
 
-console.log("digital los siguientes datos??");
-do{
-    let nombre=prompt("ingresa el nombre");
-    let sexo=prompt("ingresa el sexo");
-    let nota=parseInt(prompt("ingresa tu nota final"));
-    nombres.push(nombre);
-    genero.push(sexo);
-    final.push(nota);
-    console.log(nombres);
-    console.log(genero);
-    console.log(final);
- 
-}while(confirm("desea continuar??"))
-let mayor=Math.max(...final);
-let num=final.indexOf(mayor);
-console.log(`la nota mas alta fue la de ${nombres[num]} con una nota de ${mayor}`)
+console.log(`a nombre de ${nombre}\ncompra con un total de ${costo*cuantos}`)
