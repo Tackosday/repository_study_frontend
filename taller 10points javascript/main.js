332dafd0f2b3dc068c52ee924d336534a68720e8
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-03-14 13:30:21 -0500

    feat: :art: 10 Punto
    Desarrolle un programa cíclico que capture un dato numérico cada vez, y los vaya acumulando. El programa se detiene cuando el usuario digita un cero. El programa debe mostrar: LA SUMATORIA DE LOS VALORES, EL VALOR DEL PROMEDIO, CUÁNTOS VALORES FUERON DIGITADOS, MAYOR VALOR Y MENOR VALOR.

--- taller 10points javascript/main.js
+++ taller 10points javascript/main.js
@@ -1,18 +1,23 @@
-let atleta=[];
-let marcas=[];
-do{
-let atleta0=prompt("nombre del atleta");
-let marca0=parseFloat(prompt("cuanto fue su marca"));
-
-atleta.push(atleta0)
-marcas.push(marca0)
-console.log(atleta,marcas)
-}while(confirm("desea agregar mas datos??")){    
-
+let numeros=[];
+var num=2;
+while(num!=0){
+   var num=parseFloat(prompt("digita un numero y el 0 si quieres finalizar"));
+   if(num!=0){
+      numeros.push(num);
+   }
+   console.log(numeros)
 }
-let mayor=Math.max(...marcas)
-let uca=marcas.indexOf(mayor)
-console.log(`el ganador de los juegos olipicos de triple salto es ${atleta[uca]}`)
- if (marcas.indexOf(mayor)>15.50){
-    console.log("ademas ganaste 500 millones de pesos")
- }
+let i=0;
+var sum=0;
+const tot= numeros.length;
+
+//suma
+for(i=0;i<tot;i++){
+   sum+=numeros[i]
+}
+console.log(tot)
+console.log(`el valor de la suma es ${sum}`);
+console.log(`el promedio de los datos son ${sum/(tot)}`);
+console.log(`se digitaron ${tot} numeros sin contar el 0`);
+console.log(`el mayor de los datos fue ${Math.max(...numeros)}`);
+console.log(`el menor de los datos fue ${Math.min(...numeros)}`);
