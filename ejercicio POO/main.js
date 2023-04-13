6736b2259085a479da5ad1ad1ef6d62a421e857c
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-04-12 23:09:52 -0500

    feat: :bug: ejercicio de POO
    cargando defaults pero mas de la mitad no estan funcionando

--- ejercicio POO/main.js
+++ ejercicio POO/main.js
@@ -14,11 +14,35 @@ class lapiz{
     getColor(){
         return this.color;
     }
+    getDimension(){
+        return this.dimension;
+    }
+    getborrador(){
+        return this.borrador;
+    }
+    getMaterial(){
+        return this.material;
+    }
 }
     
 let obj = undefined;
 let color = document.querySelector(`[name="color"]`);
+let dimensiones = document.querySelector(`[name="dimension"]`);
+let borrador =document.querySelector(`[name="borrador"]`);
+// let material = document.querySelector(`[name="material"]`);
+let material = document.querySelector("tester");
+material.checked=true;
 addEventListener("DOMContentLoaded",(e)=>{
     obj=new lapiz({});
     color.value = obj.color;
-});
+    dimensiones.value = obj.dimension;
+    
+ })
+
+ 
+ function showValue(newValue) {
+    document.getElementById("rangeValue").innerHTML = newValue;
+ }
+
+ let myString = "true";
+let myBoolean = Boolean(myString);
