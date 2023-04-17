6b50121a032b9b91cce2aa747533a832297776f3
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-04-17 06:25:29 -0500

    feat: :hammer: taller4 javascript
    actualizacion del proyecto

---
+++ taller 4 javascript/main.js
@@ -0,0 +1,63 @@
+
+alert(`
+    -------------menu------------------
+    1.  lectura de dato
+    2.  crear objeto
+    3.  mostrar objeto
+    4.  crear array
+    5.  mostrar array
+    6.  elimine el primer elemento del array
+    7.  elimine el ultimo elemnto  del array
+    8.  agregar elemento al comienzo del array
+    9.  agregar elemento al final del array
+    10. crear array con objetos
+    11. interer array con objetos
+    12. interar array con objetos con FOR
+    13. interar array con forEach
+    14. interar array con objetos con map y crear copia
+    15. proseso personal
+    0.  salir`);
+ 
+
+const respuesta = prompt("digite el numero de su eleccion")
+
+switch(respuesta){
+    case "1":
+         print(respuesta); 
+        break
+    case "2":
+        
+        break
+    case "3":
+        break
+    case "4":
+        break
+    case "5":
+        break
+    case "6":
+        break
+    case "7":
+        break
+    case "8":
+        break
+    case "9":
+        break
+    case "10":
+        break
+    case "11":
+        break
+    case "12":
+        break
+    case "13":
+        break
+    case "14":
+        break
+    case "15":
+        break
+    case "0":
+        break
+    default:
+        break
+}
+
+
