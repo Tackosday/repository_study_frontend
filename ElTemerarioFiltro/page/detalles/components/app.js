2da1ba08367b2ace475457fbb85b7224dd67a923
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-05-17 00:49:32 -0500

    feat: :hammer: FILTRO
    actualizacion de eliminacion y construccion del filtro

--- ElTemerarioFiltro/page/detalles/components/app.js
+++ ElTemerarioFiltro/page/detalles/components/app.js
@@ -1,15 +1,15 @@
-// export let getCamper= async()=>{
-//     try{
-//         await fetch("http://localhost:4001/campers",{
-//             method:"get",
-//             headers:{
-//                 'Content-Type':'application/json'
-//             }
+export let deleteCamper= async(id)=>{
+    try{
+        await fetch(`http://localhost:4001/campers/${id }`,{
+            method:"DELETE",
+            headers:{
+                'Content-Type':'application/json'
+            }
 
-//         });
-//         window.location.href="index.html"
-//     }
-//     catch(error){
-//         console.log(error)
-//     }
-// }
+        });
+        window.location.href="index.html"
+    }
+    catch(error){
+        console.log(error)
+    }
+}
--- ElTemerarioFiltro/page/detalles/components/execution.js
+++ ElTemerarioFiltro/page/detalles/components/execution.js
@@ -1,36 +1,56 @@
+import { deleteCamper } from "./app.js";
 export default{
    get(){
         let url="http://localhost:4001/campers";
         fetch(url)
         .then(traduc=>{return traduc.json()})
         .then(data=>{
-            data.forEach(e => {
-                 let contain=document.querySelector("#contain")
-                contain.insertAdjacentHTML("beforeend",`
-                <div class="card" id="card">
-                <div class="imgBox">
-                    <img src="../../resources/user.png" alt="user" class="img">
-                </div>
-                <div class="text">
-                    <p class="letter name">${e.name}</p>
-                    <p class="letter cedula">${e.identificacion}</p>
-                    <p class="letter fIngreso">${e.ingreso}</p>
-                    <p class="letter skill">${e.skill}</p>
-                </div>
-                <div>
-                    <img src="../../resources/basura.png" alt="basura.png" style="width: 60px;" class="${e.id}" id="trash">
-                </div>
-                </div>
-                `)
-            });
+            let btn=document.querySelector("#check")
+            let manager=document.querySelector("#manager").value
+            switch(manager){
+                case("trainer"):
 
-            let trash= document.querySelectorAll("#trash")
-            let i;
-            for(i=0;trash.length;i++){
-                trash[i].addEventListener("click",()=>{
-                    
-                })
+                    break;
+                case("horario"):
+
+                    break;
+                case("ingreso"):
+
+                    break;
+                case("..."):
+                    data.forEach(e => {
+                        let contain=document.querySelector("#contain")
+                       contain.insertAdjacentHTML("beforeend",`
+                       <div class="card" id="card">
+                       <div class="imgBox">
+                           <img src="../../resources/user.png" alt="user" class="img">
+                       </div>
+                       <div class="text">
+                       <p class="letter ">${e.id}</p>
+                           <p class="letter name">${e.name}</p>
+                           <p class="letter cedula">${e.identificacion}</p>
+                           <p class="letter fIngreso">${e.ingreso}</p>
+                           <p class="letter skill">${e.skill}</p>
+                       </div>
+                       </div>
+                       `)
+                   });
+                    break;
             }
+
+
+
+
+
+            
+            
+            let delet = document.querySelector("#delet")
+             delet.addEventListener("click",()=>{
+                event.preventDefault();
+                let idD=document.querySelector("#value").value
+                deleteCamper(idD)
+
+             })
             })
 
             .catch(error=>{console.log(error)})
