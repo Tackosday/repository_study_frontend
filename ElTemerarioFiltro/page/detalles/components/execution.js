db7b95825691f1f32ae92789df34fa5f9dae7782
Sergio-Prada369 <alejanprada140705@gmail.com>
2023-05-17 13:38:16 -0500

    feat: :hammer: FILTRO
    actualicacion de botones de filtro

--- ElTemerarioFiltro/page/detalles/components/execution.js
+++ ElTemerarioFiltro/page/detalles/components/execution.js
@@ -1,6 +1,6 @@
 import { deleteCamper } from "./app.js";
 export default{
-   get(){
+    get(){
         let url="http://localhost:4001/campers";
         fetch(url)
         .then(traduc=>{return traduc.json()})
@@ -8,40 +8,57 @@ export default{
             
             data.forEach(e => {
                 let contain=document.querySelector("#contain")
-               contain.insertAdjacentHTML("beforeend",`
-               <div class="card" id="card">
-               <div class="imgBox">
-                   <img src="../../resources/user.png" alt="user" class="img">
-               </div>
-               <div class="text">
-               <p class="letter ">${e.id}</p>
-                   <p class="letter name">${e.name}</p>
-                   <p class="letter cedula">${e.identificacion}</p>
-                   <p class="letter fIngreso">${e.ingreso}</p>
-                   <p class="letter skill">${e.skill}</p>
-               </div>
-               </div>
-               `)
-           });
-            let trainer=document.querySelector("#trainer")
+                contain.insertAdjacentHTML("beforeend",`
+                <div class="card" id="card">
+                <div class="imgBox">
+                <img src="../../resources/user.png" alt="user" class="img">
+                </div>
+                <div class="text">
+                <p class="letter ">${e.id}</p>
+                <p class="letter name">${e.name}</p>
+                <p class="letter cedula">${e.identificacion}</p>
+                <p class="letter fIngreso">${e.ingreso}</p>
+                <p class="letter skill">${e.skill}</p>
+                </div>
+                </div>
+                `)
+            });
+
             let team=document.querySelector("#team")
+            let trainer=document.querySelector("#trainer")
             trainer.addEventListener("click",()=>{
                 let fil=document.querySelector("#fil")
                 let deside=document.querySelector("#deside")
-                deside!=null? deside.remove(): console.log("noEsta") ;   
-                insertAdjacentHTML("beforeend",`
+                deside == null? console.log("noEsta") : deside.remove();   
+                fil.insertAdjacentHTML("beforeend",`
                 <div id="deside">
                 <select name="filter">
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
+                <option value="miguel">Miguel</option>
+                <option value="yolver">Yolber</option>
+                <option value="velmer">Velmer</option>
                 </select>
-                <button>check</botton>
+                <button id="check">check</botton>
                 </div>
-                `)
+                `) 
+                let check=document.querySelector("#check")
+                check.addEventListener("click",()=>{
+                    let valor =document.querySelector(`[name=filter]`).value
+                    switch(valor){
+                        case("miguel"):
+                        break;
 
+                        case("yolver"):
+                        console.log("Y")
+                        break;
+                        case("vermer"):
+                        console.log("V")
+                        break;
+                    }
+
+                })
             })
-            team.addEventListener("click",()=>{
+
+            /* team.addEventListener("click",()=>{
                 let fil=document.querySelector("#fil")
                 let deside=document.querySelector("#deside")
                 deside!=null? deside.remove(): console.log("noEsta") ;   
@@ -57,7 +74,7 @@ export default{
                 `)
 
                 
-            })
+            }) */
             
             
                     
@@ -65,12 +82,12 @@ export default{
 
 
             let delet = document.querySelector("#delet")
-             delet.addEventListener("click",()=>{
+            delet.addEventListener("click",()=>{
                 event.preventDefault();
                 let idD=document.querySelector("#value").value
                 deleteCamper(idD)
 
-             })
+            })
             })
 
             .catch(error=>{console.log(error)})
