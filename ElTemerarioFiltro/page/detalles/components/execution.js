c4e129294635180615dbec5a1f69f7d54cbf48d0
Escalante <juandaesca5@hotmail.com>
2023-05-17 09:24:55 -0500

    feat: :hammer: FILTRO
    actualisacion de filtros

--- ElTemerarioFiltro/page/detalles/components/execution.js
+++ ElTemerarioFiltro/page/detalles/components/execution.js
@@ -5,45 +5,65 @@ export default{
         fetch(url)
         .then(traduc=>{return traduc.json()})
         .then(data=>{
-            let btn=document.querySelector("#check")
-            let manager=document.querySelector("#manager").value
-            switch(manager){
-                case("trainer"):
+            
+            data.forEach(e => {
+                let contain=document.querySelector("#contain")
+               contain.insertAdjacentHTML("beforeend",`
+               <div class="card" id="card">
+               <div class="imgBox">
+                   <img src="../../resources/user.png" alt="user" class="img">
+               </div>
+               <div class="text">
+               <p class="letter ">${e.id}</p>
+                   <p class="letter name">${e.name}</p>
+                   <p class="letter cedula">${e.identificacion}</p>
+                   <p class="letter fIngreso">${e.ingreso}</p>
+                   <p class="letter skill">${e.skill}</p>
+               </div>
+               </div>
+               `)
+           });
+            let trainer=document.querySelector("#trainer")
+            let team=document.querySelector("#team")
+            trainer.addEventListener("click",()=>{
+                let fil=document.querySelector("#fil")
+                let deside=document.querySelector("#deside")
+                deside!=null? deside.remove(): console.log("noEsta") ;   
+                insertAdjacentHTML("beforeend",`
+                <div id="deside">
+                <select name="filter">
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                </select>
+                <button>check</botton>
+                </div>
+                `)
 
-                    break;
-                case("horario"):
+            })
+            team.addEventListener("click",()=>{
+                let fil=document.querySelector("#fil")
+                let deside=document.querySelector("#deside")
+                deside!=null? deside.remove(): console.log("noEsta") ;   
+                insertAdjacentHTML("beforeend",`
+                <div id="deside">
+                <select name="filter">
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                </select>
+                <button>check</botton>
+                </div>
+                `)
 
-                    break;
-                case("ingreso"):
-
-                    break;
-                case("..."):
-                    data.forEach(e => {
-                        let contain=document.querySelector("#contain")
-                       contain.insertAdjacentHTML("beforeend",`
-                       <div class="card" id="card">
-                       <div class="imgBox">
-                           <img src="../../resources/user.png" alt="user" class="img">
-                       </div>
-                       <div class="text">
-                       <p class="letter ">${e.id}</p>
-                           <p class="letter name">${e.name}</p>
-                           <p class="letter cedula">${e.identificacion}</p>
-                           <p class="letter fIngreso">${e.ingreso}</p>
-                           <p class="letter skill">${e.skill}</p>
-                       </div>
-                       </div>
-                       `)
-                   });
-                    break;
-            }
-
-
-
-
-
+                
+            })
             
             
+                    
+            
+
+
             let delet = document.querySelector("#delet")
              delet.addEventListener("click",()=>{
                 event.preventDefault();
